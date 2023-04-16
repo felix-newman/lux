94ed71b46c364007bff3d281200ea03f38317680
Felix_Neumann <felix.maximilian.neumann@gmail.com>
2023-04-16 16:24:05 +0100

    add test setup

---
+++ test/e2e_test.py
@@ -0,0 +1,14 @@
+from luxai_s2.env import LuxAI_S2
+
+from agent import Agent
+from helpers import interact, animate
+
+if __name__ == "__main__":
+    env = LuxAI_S2()  # create the environment object
+    obs = env.reset(seed=41)  # resets an environment with a seed
+
+    agents = {player: Agent(player, env.state.env_cfg) for player in env.agents}
+    imgs = interact(env, agents, 40)
+
+    animate(imgs, fps=2, name="test")
+
